b89751460c00803a9dbdcdf59ee7589cf3e4f546
John Syrinek <john.syrinek@gmail.com>
2022-09-07 21:54:08 +0000

    tweaks

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# gathertown
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/johntron/gathertown)
+
+# TW Gather map
+
+## Running
+
+1. Grab a [Gather API key](https://app.gather.town/apikeys)
+1. Open this repo in Gitpod.io (link at top of this README)
+2. Edit src/index.ts and replace the API key with one that works
+3. Goto "Run and debug" and launch "Run server (debug)" (or press F5)
+4. Enter commands in the Debug console - e.g. `game.players`
+5. Edit src/index.ts and use autocompletion and context tips to see what properties / methods are available on `game`
+6. View src/events.proto for all the gory details
+7. View src/example-code.ts for some interesting snippets
--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,23 @@ const main = async () => {
     globalThis.game = game;
     game.subscribeToConnection((connected) => {
         console.log("connected?", connected)
-        console.log(game.players, 'a')
     });
     // Let things initialize - not sure what to wait on, but without this some data is undefined (e.g. game.players)
-    await new Promise((resolve) => setTimeout(resolve, 5000))
+    await wait(5000);
 
-    console.log(game.players)
+    console.log('Players: ', game.players)  
+
+    const id = Object.keys(game.players)[0]
+    const bot = game.players[id]
+    const target_object = game.filterObjectsInMap(bot.map, o => o)[0];
+    // game.move(1, false, 'EL571LzydnPy8YypF84UO5XY9sg1')
+    console.log(`Moving object to player ${bot.name}`)  
+    game.moveMapObject(bot.map, target_object.id, {x: bot.x, y: bot.y}, 5000)
+    await wait(5500);
+
+    console.log(`Teleporting ${bot.name} (${id}) in map ${bot.map}`);
+    game.teleport(bot.map, 54, 31, id)
 };
 main();
+
+const wait = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms))
